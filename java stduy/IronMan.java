1611047c244368efdab0f844b184c8301793091b
over1122 <lap721181@gmail.com>
2021-09-14 17:35:43 +0900

    2021/09/14 JAVA 객체 실습

---
+++ java stduy/IronMan.java
@@ -0,0 +1,7 @@
+package ch01;
+
+public class IronMan {
+   public void fly(String name) {
+        System.out.println(name + " 아이언맨이 날아다닙니다.");
+   }
+}
---
+++ java stduy/IronManTest.java
@@ -0,0 +1,11 @@
+package ch01;
+
+public class IronManTest {
+    public static void main(String[] args)
+    {
+        IronMan mark = new IronMan();
+        IronMan mark42 = new IronMan();
+        mark.fly("makr 1");
+        mark42.fly("makr 42");
+    }
+}
